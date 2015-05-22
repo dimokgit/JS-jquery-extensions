41b73daf5b78efd034a9b8f4c1542e5b6a7997bf
dimokgit <dimokdimon@gmail.com>
2015-05-22 18:09:50 -0400

    objecter

--- test.js
+++ test.js
@@ -1,6 +1,29 @@
 ﻿require(["jquery", "./src/objecter.js"], function ($, objecter) {
-  var o = { prop: "Property" };
+  var o = { prop: "Prop" };
   alert(objecter.sure(o, "Prop"));
-  alert(objecter.sure(o, "Prop2")|| "No Prop2 found");
-  alert("Using "+objecter.sure(o, { name: "Prop2", error: function (o, p, e) { alert(JSON.stringify(e)); return "DImon"; } }));
-})
+  try {
+    objecter.sure(o, "Prop2");
+  } catch (e) {
+    alert(JSON.stringify(e, ["message", "arguments", "type", "name", "stack"], 2));
+  }
+  alert("Using " + objecter.sure(o, {
+    name: "Prop3", error: function (o, p, e) {
+      alert(JSON.stringify(e)); return "DImon";
+    }
+  }));
+
+  var sureO = objecter.sure(o);
+  try {
+    sureO("Prop4");
+  } catch (e) {
+    alert(JSON.stringify(e, ["message", "arguments", "type", "name", "stack"], 2));
+  }
+  alert("Using:" + sureO({
+    name: "Prop5", error: function (o, p, e) {
+      alert(e);
+      return "hooray";
+    }
+  }));
+  alert("For Prop7 using:" + sureO({ name: "Prop7", error: "hooray2" }));
+
+});
